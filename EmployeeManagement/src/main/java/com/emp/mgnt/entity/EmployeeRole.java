3b688b0703a357e3d8630895e67e5d0506cbc7eb
Yogesh <39991349+yogesh147@users.noreply.github.com>
2022-08-19 13:22:28 +0530

    Update EmployeeRole.java

--- EmployeeManagement/src/main/java/com/emp/mgnt/entity/EmployeeRole.java
+++ EmployeeManagement/src/main/java/com/emp/mgnt/entity/EmployeeRole.java
@@ -4,18 +4,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
-@Entity 	
+@Entity
 @Data
 public class EmployeeRole {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private String id; 
-	
-	private String name; 
-	
-	private String description; 
-}
+	private String id;
 
+	private String name;
+
+	private String description;
+
+	@ManyToOne
+	private EmployeeInfo employee;
+}
