e9e9965772d1e755337676cb15321c0eefd8960b
Yogesh <39991349+yogesh147@users.noreply.github.com>
2022-08-19 13:21:59 +0530

    Update EmployeeInfo.java

--- EmployeeManagement/src/main/java/com/emp/mgnt/entity/EmployeeInfo.java
+++ EmployeeManagement/src/main/java/com/emp/mgnt/entity/EmployeeInfo.java
@@ -1,22 +1,22 @@
 package com.emp.mgnt.entity;
 
 import java.time.ZonedDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
 import org.springframework.data.annotation.CreatedDate;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -35,22 +35,21 @@ public class EmployeeInfo {
 	private Long empId;
 	
 	@NotBlank
-/*	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(
-			name = "emp_roles",
-			joinColumns = @JoinColumn(name = "emp_id"),
-			inverseJoinColumns = @JoinColumn(name = "role_id")
-			)*/
-//	private  Set<EmployeeRole> empRole = new HashSet<>();
+	/*
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "emp_roles", joinColumns = @JoinColumn(name = "emp_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<EmployeeRole> empRole = new HashSet<>();
+
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinColumn(name = "emp_id", referencedColumnName = "emp_id")
+	private Set<EmployeeDepartment> empDepartment = new HashSet<>();
+	*/
+	
 	private String empRole;
 
 	@NotBlank
 	private String empUser;
 
-//	@OneToMany(fetch = FetchType.EAGER)
-//	@JoinColumn(name = "emp_id", referencedColumnName = "emp_id")
-//	private Set<EmployeeDepartment> empDepartment = new HashSet<>();
-	
 	private String empDepartment;
 
 	@NotBlank
@@ -59,5 +58,11 @@ public class EmployeeInfo {
 	@Column(nullable = false, updatable = false)
 	@CreatedDate
 	private final ZonedDateTime createdAt = ZonedDateTime.now();
+	
+	@ManyToOne
+	private EmployeeDepartment department;
+
+	@OneToMany(mappedBy = "employee")
+	private List<EmployeeRole> roles;
 
 }
