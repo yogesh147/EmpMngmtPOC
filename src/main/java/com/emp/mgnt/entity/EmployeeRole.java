14720e5fea2ea4eb10500fef053e6348a0d16930
yogesh147 <yogeshch147@gmail.com>
2022-08-22 15:55:14 +0530

    add to string annotation

--- src/main/java/com/emp/mgnt/entity/EmployeeRole.java
+++ src/main/java/com/emp/mgnt/entity/EmployeeRole.java
@@ -9,11 +9,13 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @Table(name = "Role")
 public class EmployeeRole {
 	@Id
