bf0d87b015349b0b0b3410f0290fb92def96be6e
yogesh147 <yogeshch147@gmail.com>
2022-08-30 11:56:42 +0530

    set profile with application properties file changes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM maven:3.6.3-jdk-8
 WORKDIR /EmployeeManagement
 COPY . .
-RUN mvn clean install -DskipTests
+RUN mvn clean install -Pprod -DskipTests -X
 CMD mvn spring-boot:run
--- src/main/java/com/emp/mgnt/PocApplication.java
+++ src/main/java/com/emp/mgnt/PocApplication.java
@@ -1,26 +1,63 @@
 package com.emp.mgnt;
 
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * Employee Management POC Application's Main Page
  */
+@Slf4j
 @SpringBootApplication
-@Slf4j 
-public class PocApplication {
+public class PocApplication{
+	
+	@Autowired
+	private Environment env;
 
 	/**
 	 * main method to start application
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		SpringApplication.run(PocApplication.class, args);
+
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication app = new SpringApplication(PocApplication.class);
+		SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+		addDefaultProfile(app, source);
+		SpringApplication.run(PocApplication.class);
 		log.info("________________________________");
 		log.info("Employee Management POC APP Start");
 		log.info("________________________________");
+	}
+
+	private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+		if (!source.containsProperty("spring.profiles.active")
+				&& !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
+			app.setAdditionalProfiles("dev");
+		}
+	}
+	
+	@PostConstruct
+	public void initApplication() {
+		if (env.getActiveProfiles().length == 0) {
+			log.warn("No Spring profile configured, running with default configuration");
+		} else {
+			log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
+			Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+			if (activeProfiles.contains("dev") && activeProfiles.contains("prod")) {
+				log.error("You have misconfigured your application! "
+						+ "It should not run with both the 'dev' and 'prod' profiles at the same time.");
+			}
+		}
 	}
 
 }
